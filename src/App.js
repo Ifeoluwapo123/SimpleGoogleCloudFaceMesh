1dfbcb483b4fc01376e720718fd6910901917ddb
Ifeoluwapo123 <adenusidamilola5@gmail.com>
2021-09-13 13:43:14 +0100

    react google cloud vision api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useRef } from "react";
+import image from "./App2.jpeg";
+import vision from "react-cloud-vision-api";
+
+vision.init({ auth: "AIzaSyAVGWa5DCz5fIIc6Mv69BLTW8t83_C08ZQ" });
 
 function App() {
+  const profile = useRef();
+
+  const p = (baseImg) => {
+    return new vision.Request({
+      image: new vision.Image({
+        base64: baseImg,
+      }),
+      features: [new vision.Feature("FACE_DETECTION")],
+    });
+  };
+
+  const upload = (e) => {
+    const imgFile = e.target.files[0];
+    console.log(imgFile);
+    const reader = new FileReader();
+
+    reader.onloadend = function () {
+      let request = p(reader.result);
+
+      vision.annotate(request).then(
+        (res) => {
+          let prediction = 0;
+
+          //one single face on the image
+          if (res.responses[0].hasOwnProperty("faceAnnotations"))
+            if (res.responses[0].faceAnnotations.length == 1) prediction = 1;
+
+          console.log(prediction);
+        },
+        (e) => {
+          console.log("Error: ", e);
+        }
+      );
+    };
+
+    reader.readAsDataURL(imgFile);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        style={{ height: "50%" }}
+        ref={profile}
+        type="file"
+        onChange={upload}
+        alt=""
+      />
     </div>
   );
 }
